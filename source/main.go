566d9aec126bf9915f1f3d06c82a6678dcad9cd3
Adam Buglass <adam.buglass@C02YD87SJG5J.local>
2019-08-15 18:10:02 +0100

    Add golang lambda

---
+++ source/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda" // go get github.com/aws/aws-lambda-go/lambda
+)
+
+// HandleRequest default handler
+func HandleRequest(ctx context.Context) (string, error) {
+	return "Hello world!", nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
